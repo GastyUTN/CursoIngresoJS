6beaec6939b20b2dcb284ffc95ac6548fed5847a
GastyUTN <36900642+GastyUTN@users.noreply.github.com>
2018-07-27 11:27:24 -0300

    while

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -1,15 +1,68 @@
 function Mostrar()
 {
 
-	var contador=0
+	var contador=0;
+	var numero
+	var negativo=0
+	var positivo=0
+	var cantNeg=0
+	var cantPos=0
+	var cantCeros=0
+	var cantPares=0
+	var promNeg
+	var promPos
+	var diferencia
 
-	while(contador<10)
+	var respuesta="si";
+
+	while(respuesta!="no")
 	{
-		contador--
-		console.log(contador)
+
+		numero=prompt("ingrese un numero")
+		numero=parseInt(numero)
+
+		
+		if(numero<0)
+		{
+			negativo+=numero
+			cantNeg++
+		}
+		else if(numero>0)
+		{
+			positivo+=numero
+			cantPos++
+		}
+		else if(numero==0)
+		{
+			cantCeros++
+		}
+		else (numero %2==0  && numero!=0)
+		{
+			cantPares++
+		}
+
+
 		
 
+		respuesta=prompt("ingrese no para salir")
+		
 	}
+		promNeg=negativo/cantNeg
+		promPos=positivo/cantPos
+		diferencia=negativo+positivo
+	
+
+
+	document.write("la suma de los negativo es:"+ negativo)
+	document.write("la suma de los positivo es:"+ positivo)
+	document.write("la cantidad de los negativo es:"+ cantNeg)
+	document.write("la cantidad de los positivo es:"+ cantPos)
+	document.write("la cantidad de los ceros es:"+ cantCeros)
+	document.write("la cantidad de numero pares es:"+ cantPares)
+	document.write("el promedio de negativo es:"+ promNeg)
+	document.write("el promedio de positivo es:"+ promPos)
+	document.write("LA Diferencia es :"+ diferencia)
+
 
 
 
